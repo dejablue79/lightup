ec960cc93bd67f0393409a182b10a1e537d5c2b9
esh <esh@marketlogicsoftware.com>
2021-01-10 20:26:21 +0100

    TEST 1

--- static/js/main.js
+++ static/js/main.js
@@ -5,12 +5,11 @@ function set_status(pin, status) {
         data: {"pin": pin, "status": status},
         success: function(pin, status) {
             if (status == "on") {
-                $('#', pin).attr("src","static/images/lampon.png");
+                $('#' + pin).attr("src","static/images/lampon.png");
             } else {
-                $('#', pin).attr("src","static/images/lampoff.png");
+                $('#' + pin).attr("src","static/images/lampoff.png");
             }
-
-            console.log("pin " + pin + " was set to: " + status)
+            console.log("pin " + pin + " was set to: " + status);
         }
     });
 }
