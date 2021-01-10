a69c6d6746eeb31193f2468bc958833eb4356dc7
esh <esh@marketlogicsoftware.com>
2021-01-11 00:43:51 +0100

    alpha

--- static/js/main.js
+++ static/js/main.js
@@ -14,21 +14,21 @@ $( document ).ready(function() {
             }
         }
     });
-    function set_status(pin, status) {
-        $.ajax({
-            type: "POST",
-            url: "/light",
-            data: {"pin": pin, "status": status},
-            success: function(data) {
-                if (data["status"] === "on") {
-                    $(`#${data["pin"]}`).attr("src","static/images/lampon.png");
-                    $("#pin_" + data["pin"]).attr({ onclick:`set_status(${ data["pin"] }, 'off')`, value: "Turn off" });
-                } else {
-                    $(`#${data["pin"]}`).attr("src","static/images/lampoff.png");
-                    $(`#pin_${data["pin"]}`).attr({ onclick:`set_status(${ data["pin"] }, 'on')`, value: "Turn on" });
-                }
-                console.log("pin " + pin + " was set to: " + status);
-            }
-        });
-    }
 });
+function set_status(pin, status) {
+    $.ajax({
+        type: "POST",
+        url: "/light",
+        data: {"pin": pin, "status": status},
+        success: function(data) {
+            if (data["status"] === "on") {
+                $(`#${data["pin"]}`).attr("src","static/images/lampon.png");
+                $("#pin_" + data["pin"]).attr({ onclick:`set_status(${ data["pin"] }, 'off')`, value: "Turn off" });
+            } else {
+                $(`#${data["pin"]}`).attr("src","static/images/lampoff.png");
+                $(`#pin_${data["pin"]}`).attr({ onclick:`set_status(${ data["pin"] }, 'on')`, value: "Turn on" });
+            }
+            console.log("pin " + pin + " was set to: " + status);
+        }
+    });
+}
