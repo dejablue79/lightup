a8dc232d82b2e8f3d2b9efee78eff0cfbd1bdfb7
esh <esh@marketlogicsoftware.com>
2021-01-10 22:36:59 +0100

    alpha

--- app.py
+++ app.py
@@ -27,22 +27,28 @@ def default():
 	return render_template('lights.html', relay_pins=relay_pins, session=session)
 
 
+@app.route("/check", methods=['POST'])
+def check():
+	GPIO.setup(relay_pins, GPIO.OUT)
+	data = dict()
+	for pin in relay_pins:
+		data[pin] = GPIO.input(pin)
+		app.logger.info(f"{pin} is set to {GPIO.input(pin)}")
+	return jsonify(data)
+
 # set a route for action
 # light on or off
 @app.route("/light", methods=['POST'])
 def onAction():
 	status = request.form["status"]
-	pin = request.form["pin"]
-	GPIO.setup(int(pin), GPIO.OUT)
+	pin = int(request.form["pin"])
+	GPIO.setup(pin, GPIO.OUT)
 	if status == "on":
-		GPIO.output(int(pin), 0)
-		#message =  "Light on!"
-		print ("on")
+		GPIO.output(pin, 0)
+		print("on")
 	if status == "off":
-		pin = 1
-		GPIO.output(int(pin), 1)
-		#message = "Light off!"
-		print ("off")
+		GPIO.output(pin, 1)
+		print("off")
 	return jsonify(pin=pin, status=status)
 	
 	# return to the template with new info
--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,36 @@
+$( document ).ready(function() {
+    $.ajax({
+        type: "POST",
+        url: "/check",
+        success: function(data) {
+            for (let item in data) {
+                if (data[item] === 0) {
+                    $("#" + item).attr("src","static/images/lampon.png");
+                    $("#pin_" + item).attr("onclick",`set_status(${ item }, 'off')`);
+                    $("#pin_" + item).attr("value","Turn off");
+                } else {
+                    $("#" + item).attr("src","static/images/lampoff.png");
+                    $("#pin_" + item).attr("onclick",`set_status(${ item }, 'on')`);
+                    $("#pin_" + item).attr("value","Turn on");
+                }
+            }
+        }
+    });
+});
 function set_status(pin, status) {
     $.ajax({
         type: "POST",
         url: "/light",
         data: {"pin": pin, "status": status},
         success: function(data) {
-            if (status == "on") {
-                $(pin).attr("src","static/images/lampon.png");
+            if (data["status"] === "on") {
+                $("#" + data["pin"]).attr("src","static/images/lampon.png");
+                $("#pin_" + data["pin"]).attr("onclick",`set_status(${ data["pin"] }, 'off')`);
+                $("#pin_" + data["pin"]).attr("value","Turn off");
             } else {
-                $(pin).attr("src","static/images/lampoff.png");
+                $("#" + data["pin"]).attr("src","static/images/lampoff.png");
+                $("#pin_" + data["pin"]).attr("onclick",`set_status(${ data["pin"] }, 'on')`);
+                $("#pin_" + data["pin"]).attr("value","Turn on");
             }
             console.log("pin " + pin + " was set to: " + status);
         }
